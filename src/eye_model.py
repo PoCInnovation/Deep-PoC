b0660a41861afcb40241390e9a297589a1b39bac
Slooth <victor.guyot@epitech.eu>
2021-05-14 15:01:16 +0200

    changed learning rate

--- src/eye_model.py
+++ src/eye_model.py
@@ -32,8 +32,6 @@ class CNN(nn.Module):
         )
 
     def forward(self, x):
-        x = x.float()
-        x = x.view(1, -1, 3, img_width_down, img_height_down)
         x = self.model.forward(x)
         return x
 
@@ -41,7 +39,7 @@ class CNN(nn.Module):
         loss.backward()
 
 
-def load_image(): ## Load les image des dossier
+def get_all_images_from_directories(): ## Load les image des dossier
     fake = ["./eye_corpped_fake/" + f for f in listdir("./eye_corpped_fake") if ('.jpeg' in f)]
     real = ["./eye_corpped_real/" + f for f in listdir("./eye_corpped_real") if ('.png' in f)]
     return (fake, real)
@@ -51,7 +49,7 @@ def pil_to_tensor(image): ## Transforme une image en tensor
     img = img.view(len(image.getbands()), image.size[0], image.size[1])
     return img
 
-def tensor_rescale(tens):  ## Normalize la taille de l'image en un 200 * 40
+def tensor_rescale_normalize(tens):  ## Normalize la taille de l'image en un 200 * 40
     trans = transforms.Compose([transforms.Resize((img_width_down,img_height_down))])
     return trans(tens)
 
@@ -66,8 +64,15 @@ def create_contraste(image, color = 1, contrast = 1, brightness = 1, sharpness =
     image = enhancer.enhance(sharpness)
     return image
 
-def get_image_data_with_trans(): ## Fonction pour regrouper toute les datat en Batch //TRUC QUI RISQUE ETRE NUL
-    fake, real = load_image()
+def load_image_from_path(path_name):
+    im = Image.open(path_name)
+    im = create_contraste(im)
+    im = pil_to_tensor(im)
+    img = tensor_rescale_normalize(im)
+    return img
+
+def create_random_dataset(): ## Fonction pour regrouper toute les data en Batch //TRUC QUI RISQUE ETRE NUL
+    fake, real = get_all_images_from_directories()
     image_size = 0
     if (len(fake) - len(fake) % 64 < len(real) - len(real) % 64): ##Je recup le nombre d'image a a load, avec la possibiluter de faire des batch de 64 parfait (sans un batch d'image restante)
         image_size = len(fake) - len(fake) % 64
@@ -81,25 +86,18 @@ def get_image_data_with_trans(): ## Fonction pour regrouper toute les datat en B
     random_index = torch.randperm(image_size * 2) ##je créer des indice random
 
     for index_f in range(image_size):
-        im = Image.open(fake[index_f]) ##je load une image de deepfake
-        im = create_contraste(im)
-        im = pil_to_tensor(im)
-        img = tensor_rescale(im)
-        images_data[random_index[index_f * 2]] = img ##je stock cette image dasn une casse random du tensor qui regroupe toute les image
+        images_data[random_index[index_f * 2]] = load_image_from_path(fake[index_f])  ##je load une image de deepfake, et je stock cette image dasn une casse random du tensor qui regroupe toute les image
         expected[random_index[index_f * 2]] = 0 ##je stock au meme emplacement un 0 pour dire que c'est un fake
 
         im = Image.open(real[index_f])
-        im = create_contraste(im)
-        im = pil_to_tensor(im)
-        img = tensor_rescale(im)
-        images_data[random_index[index_f * 2 + 1]] = img##je stock cette image dasn une casse random du tensor qui regroupe toute les image
-        expected[random_index[index_f * 2 + 1]] = 1##je stock au meme emplacement un 0 pour dire que c'est un vrai
+        images_data[random_index[index_f * 2 + 1]] = load_image_from_path(real[index_f])##je load une image de real, je stock cette image dasn une casse random du tensor qui regroupe toute les image
+        expected[random_index[index_f * 2 + 1]] = 1 ##je stock au meme emplacement un 0 pour dire que c'est un vrai
 
     print(index_f)
     print(torch.tensor(images_data).size())
     return (torch.tensor(images_data).view(-1, 64, 3, 200, 40), torch.tensor(expected).view(-1, 64, 1).float())##return les valeur en avec un "resize" pour créer des btach de 64
 
-fake, expected = get_image_data_with_trans()
+fake, expected = create_random_dataset()
 print(fake.size())
 print(expected.size())
 
@@ -107,16 +105,16 @@ cnn = CNN()
 
 
 loss_model = nn.BCELoss()
-optimizer = torch.optim.Adam(cnn.model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(cnn.model.parameters(), lr=0.00001)
 for i in range(EPOCH):   ##la je train l'ia
     for i in range(fake.size()[0]):
-        output = cnn.forward(fake[i])
+        input = fake[i].float()
+        input = input.view(1, -1, 3, img_width_down, img_height_down)
+        output = cnn.forward(input)
         print(output.size())
+        print("first val:", {output[0][0]})
         loss = loss_model(output, expected[i].unsqueeze(0))
-        print(loss)
         cnn.backward(loss)
+        print(loss)
         optimizer.step()
         optimizer.zero_grad()
-
-
-##bash, adam, beautiffy, visual representation, notion
