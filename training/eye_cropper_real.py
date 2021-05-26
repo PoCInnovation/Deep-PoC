635b197a4b899773fff427046f66cbb45505068f
Slooth <victor.guyot@epitech.eu>
2021-05-26 23:10:07 +0200

    80% prediction

---
+++ training/eye_cropper_real.py
@@ -0,0 +1,30 @@
+from PIL import Image 
+import PIL
+from os import listdir
+from os.path import isfile, join
+
+##real
+mypath = './face_real_training/'
+folder_save = './eye_corpped_real_training/'
+
+left = 287
+top = 440
+right = left + 450
+bottom = top + 100
+
+def get_all_file():
+    file_list = [mypath + f for f in listdir(mypath) if ('.png' in f)]
+    file_list.sort()
+    return (file_list)
+
+def crop_image(path):
+    im = Image.open(path)
+    im = im.crop((left, top, right, bottom))
+    path = path.split("/")
+    im.save(folder_save + path[-1])
+
+image_list = get_all_file()
+
+print(image_list)
+for i in image_list:
+    crop_image(str(i))
---
+++ training/get_fake_pic.py
@@ -0,0 +1,33 @@
+from urllib import request
+from selenium import webdriver
+from os import listdir
+from os.path import isfile, join
+from time import *
+
+mypath = './face_fake_training/'
+src = 'https://thispersondoesnotexist.com/image'
+
+
+opener = request.build_opener()
+opener.addheaders=[('User-Agent','Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/36.0.1941.0 Safari/537.36')]
+request.install_opener(opener)
+
+
+def get_all_file_nbr():
+    file_list = [int(f[:-5]) for f in listdir(mypath) if ('.jpeg' in f)]
+    file_list.sort()
+    return (file_list)
+
+def create_new_image():
+    sleep(1)
+    file_id_list = get_all_file_nbr()
+    for i, elmnt in enumerate(file_id_list):
+        if (i != elmnt):
+            request.urlretrieve(src, 'face_fake_training/' + str(i) + '.jpeg')
+            print("created: ", str(i))
+            return
+    request.urlretrieve(src, 'face_fake_training/' + str(len(file_id_list)) + '.jpeg')
+    print("created: ", str(len(file_id_list)))
+
+for i in range(1000):
+    create_new_image()
