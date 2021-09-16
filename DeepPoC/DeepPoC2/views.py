29e7693b6a6179d5916d34e4738de4d177e369fc
Victor GUYOT <victor.guyot@epitech.eu>
2021-09-17 00:11:44 +0200

    feat: delete images after execution

--- DeepPoC/DeepPoC2/views.py
+++ DeepPoC/DeepPoC2/views.py
@@ -2,10 +2,20 @@ import sys
 from django.shortcuts import redirect
 from django.shortcuts import render
 from django.http import HttpResponse
-from os import path, getcwd
+from os import path, getcwd, remove
 from django.urls import reverse
 sys.path.insert(0, getcwd() + "/ai")
 from main import main_opencv
+
+def delete_images(file):
+    if path.exists(file):
+        remove(file)
+    if path.exists("eye_cv.jpeg"):
+        remove("eye_cv.jpeg")
+    if path.exists("mouth_cv.jpeg"):
+        remove("mouth_cv.jpeg")
+    if path.exists("face_cv.jpeg"):
+        remove("face_cv.jpeg")
 
 def main(request):
     if (request.method == 'POST' and request.FILES):
@@ -19,7 +29,10 @@ def main(request):
             for chunk in request.FILES['file'].chunks():
                 destination.write(chunk)
         result = main_opencv("eye_model/10.0,1.0,1.0,10", name)
-        if (result >= 0.5):
+        delete_images(name)
+        if (result == None):
+            return render(request, 'main.html', {'fileName': name, "status": "err"})
+        elif (result >= 0.5):
             return render(request, 'main.html', {'fileName': name, "status": "real"})
         else:
             return render(request, 'main.html', {'fileName': name, "status": "fake"})
